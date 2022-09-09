95c1a8448199e700aa647fd4dd38b287df170e47
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-08 22:36:19 -0500

    selection sort argorith

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorting an array in acending order
+ * @array: pointer to array
+ * @size: size of the array
+ * Return: Always 0
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int min;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[i] > array[j])
+			{
+				min = array[j];
+				array[j] = array[i];
+				array[i] = min;
+			}
+
+		}
+
+	}
+}
