a68b4456242ce485ec55efeba85210a6815d15ab
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-09-09 00:13:35 -0500

    quick sort algorith fit compile error

--- sort.h
+++ sort.h
@@ -25,4 +25,7 @@ void print_array(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void my_sort(int *array, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
+void swap(int *array, int i, int j);
 #endif
